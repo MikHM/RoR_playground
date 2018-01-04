951c70e2552256b22b4858aa683567f947e27d8e
MikHM <hemroda.martin@gmail.com>
2018-01-04 11:29:10 +0100

    REFACTOR: comments_controller, add before_action for @post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:create, :destroy]
+  before_action :set_post, only: %i[create destroy]
   http_basic_authenticate_with name: 'admin', password: 'pswadmin', only: [:destroy]
 
   def create
@@ -15,14 +15,13 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-
   private
 
   def comment_params
     params.require(:comment).permit(:user_name, :message)
   end
 
-  def set_comment
+  def set_post
     @post = Post.find(params[:post_id])
   end
 end
