94e614653ba80cf99d0ce76b68a429cd794539e5
MikHM <hemroda.martin@gmail.com>
2018-01-04 14:43:18 +0100

    FEAT: integrated skills via model generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229135941) do
+ActiveRecord::Schema.define(version: 20180104110827) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20171229135941) do
     t.string "title"
     t.text "body"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_spent_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
