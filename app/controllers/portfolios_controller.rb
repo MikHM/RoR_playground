c9829c6d72fc370337a69fae12893a81ce01aef3
MikHM <hemroda.martin@gmail.com>
2018-01-08 10:24:16 +0100

    REFACTOR: adding a before action to portfolios_controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:edit, :show, :update]
+
   def index
     @portfolios = Portfolio.all
   end
@@ -19,13 +21,11 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  def edit
-    @portfolio_item = Portfolio.find(params[:id])
-  end
+  def edit; end
+
+  def show; end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: "You Portfolio Item has been updated."}
@@ -40,5 +40,9 @@ class PortfoliosController < ApplicationController
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
+  def set_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
