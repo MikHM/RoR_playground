12018d166781bbe5a461a87a6b3674e214e3b199
MikHM <hemroda.martin@gmail.com>
2018-01-08 16:19:25 +0100

    ADD: updated status for enum, on Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108140533) do
+ActiveRecord::Schema.define(version: 20180108145127) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20180108140533) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
