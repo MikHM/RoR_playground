1580275d23d85b3117dbeb3649fbd7d50d883ec1
MikHM <hemroda.martin@gmail.com>
2018-01-08 12:19:15 +0100

    REFACTOR: Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/home'
-  get 'pages/contact'
-  get 'pages/about'
-
-  root 'application#index'
+  get 'contact' => 'pages#contact'
+  get 'about' => 'pages#about'
 
   get 'posts/index', as: 'blog'
 
@@ -12,5 +10,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  get 'apps', to: 'application#index'
+
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
