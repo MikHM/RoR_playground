8a98afc86d7f7cac37aab46f86236a0255949a88
MikHM <hemroda.martin@gmail.com>
2018-01-11 11:55:49 +0100

    add topic field on new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,7 +54,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body, :author)
+    params.require(:post).permit(:title, :body, :author, :topic_id)
   end
 
   def set_post
