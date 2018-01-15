5eb377eeb0403f7c68f7d3ea6bc3cdb7a072fb50
MikHM <hemroda.martin@gmail.com>
2018-01-15 17:25:55 +0100

    update - seed file for books

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,3 +101,15 @@ end
       biography: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
   )
 end
+
+# BOOKS
+# ###################################
+3.times do |book|
+  Bookshop::Book.create!(
+      title: "Book n°#{book}",
+      year: 2012,
+      summary: "
+Never Split the Difference: Negotiating as if Your Life Depended on It (Anglais) Broché – 23 mars 2017
+"
+  )
+end
