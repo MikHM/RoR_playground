918df4923443d7ab690ace8f1e2547d295d71183
MikHM <hemroda.martin@gmail.com>
2018-01-17 11:57:32 +0100

    add new params for Devise: new and editing account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def index
+  # TODO: need refactor, might not be best practice
+  # TODO: CHECK why I can't use "before_filter" instead of "before_action" here.
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  end
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
