858a3c4aaee0c73c4ecc99c801c9a6567b07a389
MikHM <hemroda.martin@gmail.com>
2018-01-17 14:35:57 +0100

    add controller concernes for Devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # TODO: need refactor, might not be best practice
-  # TODO: CHECK why I can't use "before_filter" instead of "before_action" here.
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  private
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
+  include DeviseWhitelist
 end
