a050295c4b6b33c65e8cfc348dcd794719dd9b68
MikHM <hemroda.martin@gmail.com>
2018-01-17 15:43:21 +0100

    add virtual attributes to User model

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -9,7 +9,7 @@ module DeviseWhitelist
   private
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys[:name])
-    devise_parameter_sanitizer.permit(:account_update, keys[:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
