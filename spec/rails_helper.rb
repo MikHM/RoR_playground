9ca5d5a84d5c80e48a84ce972233ea8b7d658f3d
MikHM <hemroda.martin@gmail.com>
2018-01-25 10:23:29 +0100

    fix rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,12 +2,6 @@
 require 'spec_helper'
 require 'shoulda/matchers'
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework: rspec
-    with.library: rails
-  end
-end
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -63,4 +57,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
